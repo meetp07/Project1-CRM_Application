3de7dc40024a7d4f100813800a70e372c05a8c76
meetp07 <meetpatelg3@gmail.com>
2024-03-16 19:34:25 +0530

    Final touch up, Completed Project

--- crm/webapp/forms.py
+++ crm/webapp/forms.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Record
 
@@ -7,29 +7,36 @@ from django import forms
 
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
-# Register/Create a user
 
+# - Register/Create a user
 class CreateUserForm(UserCreationForm):
+
     class Meta:
+
         model = User
         fields = ['username', 'password1', 'password2']
 
-# Login/Authenticate a user
-        
+# - Login a user
+
 class LoginForm(AuthenticationForm):
+
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
 
-# Create a Record
+# - Create a record
 
 class CreateRecordForm(forms.ModelForm):
+
     class Meta:
+
         model = Record
         fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
 
-# Update a Record
+# - Update a record
 
 class UpdateRecordForm(forms.ModelForm):
+
     class Meta:
+
         model = Record
         fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
